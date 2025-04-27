ca938a09b58e4ba5e3761632393cc31b74ce3757
juancruzCT <juancruzctclases12@gmail.com>
2025-04-26 21:29:37 -0300

    login inicial

---
+++ modularapp/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "100px" }}>
+        <h1>¡Bienvenido a la página principal!</h1>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
